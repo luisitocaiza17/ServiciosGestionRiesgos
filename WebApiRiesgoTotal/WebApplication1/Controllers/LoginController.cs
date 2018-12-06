815e8a1d7a28ec9a7f4f87b25e683c36b311d0cc
luisito caiza <luisito.caiza17@hotmail.com>
2018-12-06 18:40:02 -0500

    nueva version

---
+++ WebApiRiesgoTotal/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Collections.Generic;
+using WebApplication1.Entidades;
+
+namespace Saludsa.WebApiBrokers.Controllers
+{
+    /// <summary>
+    /// url para probar el servicio http://localhost:4259/WebDemo/swagger/ui/index#/
+    /// Servicios de Usuario
+    /// </summary>
+    /// <seealso cref="ApiController" />
+    [RoutePrefix("api/Login")]
+    public class LoginController : ApiController
+    {
+        /// <summary>
+        /// En la cabezaran van los parametros que se solicitaran
+        /// </summary>
+        /// <param name="usuario">Cabecera de la llamada</param>
+        /// <param name="password"></param>
+        /// <returns>
+        /// HttpResponseMessage
+        /// </returns>
+        /// <response code="200">Si se ejecuta con éxito y retorna el valor esperado</response>
+        /// <response code="400">Error datos enviados en la cabecera</response>
+        /// <response code="500">Si existe un error interno</response>
+        [Route("LoginVerificacion")]
+        [HttpGet()]
+
+        [ResponseType(typeof(string))]
+        public HttpResponseMessage LoginVerificacion(string usuario, string password)
+        {
+            UsuarioEntity usuarioComprobado = new UsuarioEntity();
+            Msg respuesta = new Msg();
+            if (usuario.Equals("admin") && password.Equals("123456"))
+            {
+
+                usuarioComprobado.id = 1;
+                usuarioComprobado.nombres = "administrador";
+                usuarioComprobado.apellidos = "pruebas";
+                
+            }
+            respuesta.Datos = usuarioComprobado;
+            respuesta.Estado = "OK";
+            respuesta.Mensajes = "Correcto";
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, respuesta);
+        }
+
+    }
+}
---
+++ WebApiRiesgoTotal/WebApplication1/Entidades/UsuarioEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entidades
+{
+    public class UsuarioEntity
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+    }
+
+    class Msg
+    {
+
+        public string Estado { get; set; }
+        public object Datos { get; set; }
+        public string Mensajes { get; set; }
+    
+     }
+
+}
